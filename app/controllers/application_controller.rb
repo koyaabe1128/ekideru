49081aeeeed0255d454a1900504773a19f439579
koyaabe1128 <koyatendo@gmail.com>
2021-09-27 05:23:04 +0000

    Add name column to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  
   def after_sign_in_path_for(resource)
     search_places_path
   end
@@ -6,4 +12,13 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+  
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+  
 end
