63282399138a2ed6097954b27788a411bba780ed
koyaabe1128 <koyatendo@gmail.com>
2021-09-29 07:38:09 +0000

    Update places/search

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,6 +3,6 @@ class Place < ApplicationRecord
   validates :image, presence: true
   validates :kind, presence: true
   validates :exit_num, presence: true
-
+  
   # kind(building -> 0, landmark -> 1)
 end
