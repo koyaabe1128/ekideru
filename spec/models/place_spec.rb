014403107ad735385987fc11ee19630a1aa4652f
koyaabe1128 <koyatendo@gmail.com>
2021-10-10 17:15:53 +0000

    Update spec/models/user_spec

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Place, type: :model do
+RSpec.describe User, type: :model do
   describe "新規登録" do
     context "登録成功" do
       it "有効" do
@@ -12,43 +12,24 @@ RSpec.describe Place, type: :model do
     context "登録失敗" do
       it "nameが空だと登録できない" do
         place = build(:place, name: "")
-        place.valid?
-        expect(place.errors.full_messages).to include("Name can't be blank")
+        expect(place).to be_invalid
       end
       it "nameが３０文字を超えて登録できない" do
         place = build(:place, name: "a" * 31) 
-        place.valid?
-        expect(place.errors.full_messages).to include("Name is too long (maximum is 30 characters)")
+        expect(place).to be_invalid
       end
-      # it "nameが重複していて無効" do
-      #   place = build(:place)
-      #   place.save
-      #   another_place = build(:place, yomigana: "あああああ")
-      #   another_place.valid?
-      #   expect(another_place.errors.full_messages).to include("")
-      # end
       it "yomiganaが空だと登録できない" do
         place = build(:place, yomigana: "")
-        place.valid?
-        expect(place.errors.full_messages).to include("Yomigana can't be blank")
+        expect(place).to be_invalid
       end
       it "yomiganaが７０文字を超えて登録できない" do
         place = build(:place, yomigana: "a" * 71) 
-        place.valid?
-        expect(place.errors.full_messages).to include("Yomigana is too long (maximum is 70 characters)")
+        expect(place).to be_invalid
       end
       it "yomiganaがひらがなでないと登録できない" do
         place = build(:place, yomigana: "アアアアア") 
-        place.valid?
-        expect(place.errors.full_messages).to include("Yomigana is invalid")
+        expect(place).to be_invalid
       end
-      # it "yomiganaが重複していて無効" do
-      #   place = build(:place)
-      #   place.save
-      #   another_place = build(:place, name: "あああああ")
-      #   another_place.valid?
-      #   expect(another_place.errors.full_messages).to include("")
-      # end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,54 @@
 require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "新規登録" do
+    context "登録成功" do
+      it "有効" do
+        user = build(:user)
+        expect(user).to be_valid
+      end 
+    end
+    
+    context "登録失敗" do
+      it "nameが空だと登録できない" do
+        user = build(:user, name: "")
+        expect(user).to be_invalid
+      end
+      it "nameが３０文字を超えて登録できない" do
+        user = build(:user, name: "a" * 31)
+        expect(user).to be_invalid
+      end
+      it "emailが空だと登録できない" do
+        user = build(:user, email: "")
+        expect(user).to be_invalid
+      end
+      it "emailが重複していると登録できない" do
+        user = build(:user)
+        user.save
+        another_user = build(:user, password: "654321")
+        another_user.valid?
+        expect(another_user).to be_invalid
+      end
+      it "passwordが空だと登録できない" do
+        user = build(:user, password: "")
+        expect(user).to be_invalid
+      end
+      it "passwordが全角だと登録できない" do
+        user = build(:user, password: "ああああああ")
+        expect(user).to be_invalid
+      end
+      it "passwordが６文字未満だと登録できない" do
+        user = build(:user, password: "12345")
+        expect(user).to be_invalid
+      end
+      it "passwordが１２８文字以上だと登録できない" do
+        user = build(:user, password: "1" * 129)
+        expect(user).to be_invalid
+      end
+      it "password_confirmationが空、passwordと不一致だと登録できない" do
+        user = build(:user, password_confirmation: "")
+        expect(user).to be_invalid
+      end
+    end
+  end
+end
